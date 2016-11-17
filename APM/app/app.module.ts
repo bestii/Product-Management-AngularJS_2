98b4b362934524121a09fb97049ae73f8f131d05
Bestin john <bjohn@mcfadyen.com>
2016-11-17 18:30:09 +0530

    Completed Half of Retrieving Data Using HTTP till Sending an HTTP Request

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { AppComponent }  from './app.component';
-import {ProductListComponent} from './products/product-list.component'
-import {ProductFilterPipe} from './products/product-filter.pipe'
-import {StarComponent} from './shared/star.component'
+import {ProductListComponent} from './products/product-list.component';
+import {ProductFilterPipe} from './products/product-filter.pipe';
+import {StarComponent} from './shared/star.component';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule,
+              FormsModule,
+              HttpModule],
     declarations: [AppComponent,
                    ProductListComponent,
                    ProductFilterPipe,
